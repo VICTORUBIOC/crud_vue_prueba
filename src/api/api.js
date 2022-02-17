e19c113a6d7216aa3386e45bf21fd2d9659c2a7a
VictoRubioC <vrubioc@ucacue.edu.ec>
2022-02-17 09:30:09 -0500

    Agragar Docker

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 
 
 Vue.prototype.$axios = axios.create({
-  baseURL: 'http://localhost:8081/api', 
+  baseURL: 'http://157.245.188.57:8081/api', 
   headers: {
     'Accept':' application/json',
     'Content-Type': 'application/json'
